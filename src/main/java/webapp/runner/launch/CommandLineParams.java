17ca17277d7d501c3dbe1039a6acaa54103bd132
Bhagwat Kumar <bhagwat@intelligrape.com>
2014-04-04 16:26:36 +0530

    added bindOnInit as command line parameter with default value true

--- src/main/java/webapp/runner/launch/CommandLineParams.java
+++ src/main/java/webapp/runner/launch/CommandLineParams.java
@@ -3,14 +3,12 @@ package webapp.runner.launch;
 import com.beust.jcommander.Parameter;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
  * Holds the command line parameters
- * 
+ *
  * @author jamesward
- * 
  */
 
 public class CommandLineParams {
@@ -20,37 +18,40 @@ public class CommandLineParams {
 
     @Parameter(names = "--session-timeout", description = "The number of minutes of inactivity before a user's session is timed out.")
     public Integer sessionTimeout;
-    
+
     @Parameter(names = "--port", description = "The port that the server will accept http requests on.")
-    public Integer port = 8080;    
+    public Integer port = 8080;
+
+    @Parameter(names = "--bindOnInit", description = "If set to false, the socket will be bound when the connector is started and unbound when it is stopped.")
+    public String bindOnInit = "true";
 
     @Parameter(names = "--context-xml", description = "The path to the context xml to use.")
     public String contextXml;
 
     @Parameter(names = "--path", description = "The context path")
     public String contextPath = "";
-    
+
     @Parameter(names = "--shutdown-override", description = "Overrides the default behavior and casues Tomcat to ignore lifecycle failure events rather than shutting down when they occur.")
     public boolean shutdownOverride = false;
-    
+
     @Parameter(names = "--session-store", description = "Session store to use (valid options are 'memcache' or 'redis')")
     public String sessionStore;
-    
+
     @Parameter(names = "--session-store-operation-timeout", description = "Operation timeout for the memcache session store. (default is 5000ms)")
     public Integer sessionStoreOperationTimout = 5000;
-    
+
     @Parameter(names = "--session-store-locking-mode", description = "Session locking mode for use with memcache session store. (default is all)")
     public String sessionStoreLockingMode = "all";
-    
+
     @Parameter(names = "--session-store-ignore-pattern", description = "Request pattern to not track sessions for. Valid only with memcache session store. (default is '.*\\.(png|gif|jpg|css|js)$'")
     public String sessionStoreIgnorePattern = ".*\\.(png|gif|jpg|css|js)$";
 
     @Parameter(names = "--help", help = true)
     public boolean help;
-    
+
     @Parameter(names = "--enable-compression", description = "Enable GZIP compression on responses")
     public boolean enableCompression;
-    
+
     @Parameter(names = "--compressable-mime-types", description = "Comma delimited list of mime types that will be compressed when using GZIP compression.")
     public String compressableMimeTypes = "text/html,text/xml,text/plain,text/css,application/json,application/xml,text/javascript,application/javascript";
 
@@ -62,16 +63,16 @@ public class CommandLineParams {
 
     @Parameter(names = "--enable-basic-auth", description = "Secure the app with basic auth. Use with --basic-auth-user and --basic-auth-pw or --tomcat-users-location")
     public boolean enableBasicAuth = false;
-    
+
     @Parameter(names = "--basic-auth-user", description = "Username to be used with basic auth. Defaults to BASIC_AUTH_USER env variable.")
     public String basicAuthUser;
-    
+
     @Parameter(names = "--basic-auth-pw", description = "Password to be used with basic auth. Defaults to BASIC_AUTH_PW env variable.")
     public String basicAuthPw;
-    
+
     @Parameter(names = "--tomcat-users-location", description = "Location of the tomcat-users.xml file. (relative to the location of the webapp-runner jar file)")
     public String tomcatUsersLocation;
-    
+
     @Parameter(names = "--expand-war", description = "Expand the war file and set it as source")
     public boolean expandWar = false;
 }
--- src/main/java/webapp/runner/launch/Main.java
+++ src/main/java/webapp/runner/launch/Main.java
@@ -25,22 +25,8 @@
  */
 package webapp.runner.launch;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-
-import javax.naming.CompositeName;
-import javax.naming.StringRefAddr;
-import javax.servlet.annotation.ServletSecurity.TransportGuarantee;
-
-import org.apache.catalina.Context;
-import org.apache.catalina.Globals;
-import org.apache.catalina.LifecycleEvent;
-import org.apache.catalina.LifecycleException;
-import org.apache.catalina.LifecycleListener;
-import org.apache.catalina.LifecycleState;
-import org.apache.catalina.Role;
-import org.apache.catalina.Server;
+import com.beust.jcommander.JCommander;
+import org.apache.catalina.*;
 import org.apache.catalina.connector.Connector;
 import org.apache.catalina.core.StandardServer;
 import org.apache.catalina.deploy.LoginConfig;
@@ -51,16 +37,20 @@ import org.apache.catalina.startup.Tomcat;
 import org.apache.catalina.users.MemoryUserDatabase;
 import org.apache.catalina.users.MemoryUserDatabaseFactory;
 
-import com.beust.jcommander.JCommander;
+import javax.naming.CompositeName;
+import javax.naming.StringRefAddr;
+import javax.servlet.annotation.ServletSecurity.TransportGuarantee;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
 
 /**
  * This is the main entry point to webapp-runner. Helpers are called to parse the arguments.
  * Tomcat configuration and launching takes place here.
- *
  */
 public class Main {
-    
+
     private static final String AUTH_ROLE = "user";
 
     public static void main(String[] args) throws Exception {
@@ -73,10 +63,10 @@ public class Main {
             jCommander.usage();
             System.exit(1);
         }
-        
+
         // default to src/main/webapp
         if (commandLineParams.paths.size() == 0) {
-             commandLineParams.paths.add("src/main/webapp");
+            commandLineParams.paths.add("src/main/webapp");
         }
 
         final Tomcat tomcat = new Tomcat();
@@ -89,32 +79,36 @@ public class Main {
         nioConnector.setPort(commandLineParams.port);
         nioConnector.setURIEncoding("UTF-8");
 
-		if (commandLineParams.enableSSL) {
-			nioConnector.setSecure(true);
-			nioConnector.setProperty("SSLEnabled", "true");
-			String pathToTrustStore = System.getProperty("javax.net.ssl.trustStore");
-			if (pathToTrustStore != null) {
-				nioConnector.setProperty("sslProtocol", "tls");
-				File truststoreFile = new File(pathToTrustStore);
-				nioConnector.setAttribute("truststoreFile", truststoreFile.getAbsolutePath());
-				System.out.println(truststoreFile.getAbsolutePath());
-				nioConnector.setAttribute("trustStorePassword", System.getProperty("javax.net.ssl.trustStorePassword"));
-			}
-			String pathToKeystore = System.getProperty("javax.net.ssl.keyStore");
-			if (pathToKeystore != null) {
-				File keystoreFile = new File(pathToKeystore);
-				nioConnector.setAttribute("keystoreFile", keystoreFile.getAbsolutePath());
-				System.out.println(keystoreFile.getAbsolutePath());
-				nioConnector.setAttribute("keystorePass", System.getProperty("javax.net.ssl.keyStorePassword"));
-			}
-			if (commandLineParams.enableClientAuth) {
-				nioConnector.setAttribute("clientAuth", true);
-			}
-		}
+        if (commandLineParams.bindOnInit.equalsIgnoreCase("false")) {
+            nioConnector.setProperty("bindOnInit", "false");
+        }
 
-        if(commandLineParams.enableCompression) {
-        	nioConnector.setProperty("compression", "on");
-        	nioConnector.setProperty("compressableMimeType", commandLineParams.compressableMimeTypes);
+        if (commandLineParams.enableSSL) {
+            nioConnector.setSecure(true);
+            nioConnector.setProperty("SSLEnabled", "true");
+            String pathToTrustStore = System.getProperty("javax.net.ssl.trustStore");
+            if (pathToTrustStore != null) {
+                nioConnector.setProperty("sslProtocol", "tls");
+                File truststoreFile = new File(pathToTrustStore);
+                nioConnector.setAttribute("truststoreFile", truststoreFile.getAbsolutePath());
+                System.out.println(truststoreFile.getAbsolutePath());
+                nioConnector.setAttribute("trustStorePassword", System.getProperty("javax.net.ssl.trustStorePassword"));
+            }
+            String pathToKeystore = System.getProperty("javax.net.ssl.keyStore");
+            if (pathToKeystore != null) {
+                File keystoreFile = new File(pathToKeystore);
+                nioConnector.setAttribute("keystoreFile", keystoreFile.getAbsolutePath());
+                System.out.println(keystoreFile.getAbsolutePath());
+                nioConnector.setAttribute("keystorePass", System.getProperty("javax.net.ssl.keyStorePassword"));
+            }
+            if (commandLineParams.enableClientAuth) {
+                nioConnector.setAttribute("clientAuth", true);
+            }
+        }
+
+        if (commandLineParams.enableCompression) {
+            nioConnector.setProperty("compression", "on");
+            nioConnector.setProperty("compressableMimeType", commandLineParams.compressableMimeTypes);
         }
 
         tomcat.setConnector(nioConnector);
@@ -122,35 +116,35 @@ public class Main {
         tomcat.getService().addConnector(tomcat.getConnector());
 
         tomcat.setPort(commandLineParams.port);
-        
+
         if (commandLineParams.paths.size() > 1) {
             System.out.println("WARNING: multiple paths are specified, but no longer supported. First path will be used.");
         }
-        
+
         // Get the first path
         String path = commandLineParams.paths.get(0);
 
         Context ctx = null;
-        
+
         File war = new File(path);
-        
+
         if (!war.exists()) {
             System.err.println("The specified path \"" + path + "\" does not exist.");
             jCommander.usage();
             System.exit(1);
         }
-        
+
         // Use the commandline context-path (or default)
         // warn if the contextPath doesn't start with a '/'. This causes issues serving content at the context root.
         if (commandLineParams.contextPath.length() > 0 && !commandLineParams.contextPath.startsWith("/")) {
             System.out.println("WARNING: You entered a path: [" + commandLineParams.contextPath + "]. Your path should start with a '/'. Tomcat will update this for you, but you may still experience issues.");
         }
-        
+
         final String ctxName = commandLineParams.contextPath;
-            
-        if (commandLineParams.expandWar && war.isFile()){
+
+        if (commandLineParams.expandWar && war.isFile()) {
             File appBase = new File(System.getProperty(Globals.CATALINA_BASE_PROP), tomcat.getHost().getAppBase());
-            if (appBase.exists()){
+            if (appBase.exists()) {
                 appBase.delete();
             }
             appBase.mkdir();
@@ -164,8 +158,8 @@ public class Main {
             System.out.println("Adding Context " + ctxName + " for " + war.getPath());
             ctx = tomcat.addWebapp(ctxName, war.getAbsolutePath());
         }
-          
-        if(!commandLineParams.shutdownOverride) {          
+
+        if (!commandLineParams.shutdownOverride) {
             // allow Tomcat to shutdown if a context failure is detected
             ctx.addLifecycleListener(new LifecycleListener() {
                 public void lifecycleEvent(LifecycleEvent event) {
@@ -179,11 +173,10 @@ public class Main {
                 }
             });
         }
-        
-        
-        
+
+
         // set the context xml location if there is only one war
-        if(commandLineParams.contextXml != null) {
+        if (commandLineParams.contextXml != null) {
             System.out.println("Using context config: " + commandLineParams.contextXml);
             ctx.setConfigFile(new File(commandLineParams.contextXml).toURI().toURL());
         }
@@ -194,20 +187,20 @@ public class Main {
         }
 
         //set the session timeout
-        if(commandLineParams.sessionTimeout != null) {
+        if (commandLineParams.sessionTimeout != null) {
             ctx.setSessionTimeout(commandLineParams.sessionTimeout);
         }
 
         addShutdownHook(tomcat);
-        
+
         if (commandLineParams.enableBasicAuth || commandLineParams.tomcatUsersLocation != null) {
             tomcat.enableNaming();
         }
-        
+
         if (commandLineParams.enableBasicAuth) {
             enableBasicAuth(ctx, commandLineParams.enableSSL);
         }
-        
+
         //start the server
         tomcat.start();
 
@@ -221,7 +214,7 @@ public class Main {
         if (commandLineParams.enableBasicAuth || commandLineParams.tomcatUsersLocation != null) {
             configureUserStore(tomcat, commandLineParams);
         }
-        
+
         commandLineParams = null;
 
         tomcat.getServer().await();
@@ -247,7 +240,7 @@ public class Main {
             return baseDir.getAbsolutePath();
         }
     }
-    
+
     /*
      * Set up basic auth security on the entire application
      */
@@ -256,10 +249,10 @@ public class Main {
         loginConfig.setAuthMethod("BASIC");
         ctx.setLoginConfig(loginConfig);
         ctx.addSecurityRole(AUTH_ROLE);
-        
+
         SecurityConstraint securityConstraint = new SecurityConstraint();
         securityConstraint.addAuthRole(AUTH_ROLE);
-        if(enableSSL) {            
+        if (enableSSL) {
             securityConstraint.setUserConstraint(TransportGuarantee.CONFIDENTIAL.toString());
         }
         SecurityCollection securityCollection = new SecurityCollection();
@@ -267,44 +260,44 @@ public class Main {
         securityConstraint.addCollection(securityCollection);
         ctx.addConstraint(securityConstraint);
     }
-    
+
     static void configureUserStore(final Tomcat tomcat, final CommandLineParams commandLineParams) throws Exception {
         String tomcatUsersLocation = commandLineParams.tomcatUsersLocation;
-        if(tomcatUsersLocation == null) {
+        if (tomcatUsersLocation == null) {
             tomcatUsersLocation = "../../tomcat-users.xml";
         }
-        
+
         javax.naming.Reference ref = new javax.naming.Reference("org.apache.catalina.UserDatabase");
         ref.add(new StringRefAddr("pathname", tomcatUsersLocation));
-        MemoryUserDatabase memoryUserDatabase = 
+        MemoryUserDatabase memoryUserDatabase =
                 (MemoryUserDatabase) new MemoryUserDatabaseFactory().getObjectInstance(
-                ref,
-                new CompositeName("UserDatabase"),
-                null,
-                null);
+                        ref,
+                        new CompositeName("UserDatabase"),
+                        null,
+                        null);
 
         // Add basic auth user
-        if(commandLineParams.basicAuthUser != null && commandLineParams.basicAuthPw != null) {
-            
+        if (commandLineParams.basicAuthUser != null && commandLineParams.basicAuthPw != null) {
+
             memoryUserDatabase.setReadonly(false);
             Role user = memoryUserDatabase.createRole(AUTH_ROLE, AUTH_ROLE);
             memoryUserDatabase.createUser(
-                    commandLineParams.basicAuthUser, 
-                    commandLineParams.basicAuthPw, 
+                    commandLineParams.basicAuthUser,
+                    commandLineParams.basicAuthPw,
                     commandLineParams.basicAuthUser).addRole(user);
             memoryUserDatabase.save();
-            
+
         } else if (System.getenv("BASIC_AUTH_USER") != null && System.getenv("BASIC_AUTH_PW") != null) {
-            
+
             memoryUserDatabase.setReadonly(false);
             Role user = memoryUserDatabase.createRole(AUTH_ROLE, AUTH_ROLE);
             memoryUserDatabase.createUser(
-                    System.getenv("BASIC_AUTH_USER"), 
-                    System.getenv("BASIC_AUTH_PW"), 
+                    System.getenv("BASIC_AUTH_USER"),
+                    System.getenv("BASIC_AUTH_PW"),
                     System.getenv("BASIC_AUTH_USER")).addRole(user);
             memoryUserDatabase.save();
         }
-        
+
         // Register memoryUserDatabase with GlobalNamingContext
         System.out.println("MemoryUserDatabase: " + memoryUserDatabase);
         tomcat.getServer().getGlobalNamingContext().addToEnvironment("UserDatabase", memoryUserDatabase);
@@ -318,14 +311,14 @@ public class Main {
         ctxRes.setProperty("factory", "org.apache.catalina.users.MemoryUserDatabaseFactory");
         ctxRes.setProperty("pathname", tomcatUsersLocation);
         tomcat.getServer().getGlobalNamingResources().addResource(ctxRes);
-        tomcat.getEngine().setRealm(new org.apache.catalina.realm.UserDatabaseRealm());       
+        tomcat.getEngine().setRealm(new org.apache.catalina.realm.UserDatabaseRealm());
     }
-    
+
     /**
      * Stops the embedded Tomcat server.
      */
     static void addShutdownHook(final Tomcat tomcat) {
-        
+
         // add shutdown hook to stop server
         Runtime.getRuntime().addShutdownHook(new Thread() {
             public void run() {
@@ -337,6 +330,6 @@ public class Main {
                     throw new RuntimeException("WARNING: Cannot Stop Tomcat " + exception.getMessage(), exception);
                 }
             }
-        });    
+        });
     }
 }
